75995b24b1cbbde0a61ae39c234395bbf24a9cf3
Chris Fordham <chris@xhost.com.au>
2014-01-21 19:54:31 +1100

    Remove uneeded whitespace.

--- providers/hostname.rb
+++ providers/hostname.rb
@@ -19,7 +19,7 @@
 action :set do
 
   require 'socket'
-  
+
   def local_ip
     # turn off reverse DNS resolution temporarily
     orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
@@ -30,14 +30,14 @@ action :set do
     ensure
       Socket.do_not_reverse_lookup = orig
   end
-  
+
   # get node IP
   node_ip = local_ip
   log("Node IP: #{node_ip}") { level :debug }
-  
+
   # ensure the required short hostname is lower case
   new_resource.short_hostname.downcase!
-  
+
   # set hostname from short or long (when domain_name set)
   unless new_resource.domain_name.nil? || new_resource.domain_name == ''
     hostname = "#{new_resource.short_hostname}.#{new_resource.domain_name}"
@@ -47,7 +47,7 @@ action :set do
     hosts_list = new_resource.short_hostname
   end
   log("Setting hostname for '#{hostname}'.") { level :debug }
-  
+
   # Update /etc/hosts
   log('Configure /etc/hosts.') { level :debug }
   template '/etc/hosts' do
@@ -59,7 +59,7 @@ action :set do
       )
     mode 0744
   end
-  
+
   # Update /etc/hostname
   log('Configure /etc/hostname') { level :debug }
   file '/etc/hostname' do
@@ -69,7 +69,7 @@ action :set do
     content new_resource.short_hostname
     action :create
   end
-  
+
   # Call hostname command
   log('Setting hostname.') { level :debug }
   if platform?('centos', 'redhat')
@@ -86,7 +86,7 @@ action :set do
       EOH
     end
   end
-  
+
   # Call domainname command
   if node['platform'] != 'archlinux' and ( !new_resource.domain_name.nil? || new_resource.domain_name != "" )
     log('Running domainname') { level :debug }
@@ -96,7 +96,7 @@ action :set do
       EOH
     end
   end
-  
+
   # restart hostname services on appropriate platforms
   if platform?('ubuntu')
     log('Starting hostname service.') { level :debug }
@@ -114,7 +114,7 @@ action :set do
     action :start
     end
   end
-  
+
   # rightlink commandline tools set tag with rs_tag
   # the rs_tag command exits 127 or similar on occasion, though not from command line (not sure why); hopefully || true can be removed in the future
   script 'set node hostname tag' do
@@ -124,20 +124,20 @@ action :set do
   ( if type -P rs_tag &>/dev/null; then rs_tag --add 'node:hostname=#{hostname}'; fi ) || true
     EOH
   end
-  
+
   # reload ohai hostname plugin for subsequent recipes in the run_list, or not (http://tickets.opscode.com/browse/OHAI-389?focusedCommentId=26255&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-26255)
   #ohai "reload_hostname_info_from_ohai" do
   #  plugin "hostname"
   #end
-  
+
   # manually update node & automatic attributes (probably won't do anything heh)
   node.automatic_attrs['hostname'] = `hostname -f`.strip
   node.automatic_attrs['fqdn'] = `hostname -f`.strip
   node.set['fqdn'] = `hostname -f`.strip
   node.set['hostname'] = `hostname -f`.strip
-  
+
   #node.save
-  
+
   # Show the new host/node information (after ohai reload from provider)
   ruby_block "show_host_info" do
     block do
@@ -153,7 +153,7 @@ action :set do
       Chef::Log.info("Current Chef FQDN loaded from Ohai: #{node['fqdn']}")
     end
   end
-  
+
   new_resource.updated_by_last_action(true)
 
 end # close action :set
