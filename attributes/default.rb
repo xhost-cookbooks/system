051157c3fbfbd0c0c1a6e3f5b56aba0a03514859
Chris Fordham <chris@xhost.com.au>
2014-02-27 11:47:38 +1100

    Default system/short_hostname to node['hostname'].

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 
 default['system']['timezone'] = 'UTC'
-default['system']['short_hostname'] = 'localhost'
+default['system']['short_hostname'] = node['hostname']
 default['system']['domain_name'] = 'localdomain'
 default['system']['static_hosts'] = {}
 default['system']['upgrade_packages'] = true
