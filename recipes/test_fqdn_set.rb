b0a3fe73d4c2f7ff93974347ad6fa8b6da4a7ec5
Chris Fordham <chris@xhost.com.au>
2015-02-25 13:52:59 +1100

    Add a recipe to test setting of the fqdn.

---
+++ recipes/test_fqdn_set.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+#
+# Cookbook Name:: system
+# Recipe:: test_fqdn_template
+#
+# Copyright 2012-2015, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+template '/tmp/test_fqdn_set.txt' do
+  source 'test_fqdn_set.erb'
+  variables lazy {
+    {
+      fqdn: node['fqdn']
+    }
+  }
+end
+
+log 'lazy_log_fqdn' do
+  message lazy { node['fqdn'] }
+end
