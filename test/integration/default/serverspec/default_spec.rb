e8256684de4db94591375595209a0b3dc9fd0bba
Chris Fordham <chris.fordham@industrieit.com>
2015-05-18 21:32:43 +1000

    Add Serverspec file test for issue #14.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,19 +2,34 @@
 
 require_relative 'spec_helper'
 
+# /etc/hostname
 describe file('/etc/hostname') do
   it { should be_file }
 end
 
-describe file('/etc/timezone') do
-  it { should be_file }
+describe file('/etc/hostname') do
+  it { should be_owned_by 'root' }
 end
 
-describe file('/etc/timezone') do
-  its(:content) { should contain 'Australia/Sydney' }
+# /etc/timezone (debian family only)
+if (os[:family] == 'debian') || (os[:family] == 'ubuntu')
+  describe file('/etc/timezone') do
+    it { should be_file }
+  end
+
+  describe file('/etc/timezone') do
+    it { should be_owned_by 'root' }
+  end
+
+  describe file('/etc/timezone') do
+    its(:content) { should contain 'Australia/Sydney' }
+  end
 end
 
 # TODO: /etc/localtime conditional tests
+describe file('/etc/localtime') do
+  it { should be_owned_by 'root' }
+end
 
 describe host('localhost') do
   it { should be_resolvable.by('localhost') }
@@ -34,15 +49,12 @@ describe host('test.kitchen') do
   it { should be_reachable }
 end
 
-# file ownership checks
-describe file('/etc/hostname') do
-  it { should be_owned_by 'root' }
-end
-
-describe file('/etc/timezone') do
-  it { should be_owned_by 'root' }
-end
-
-describe file('/etc/localtime') do
-  it { should be_owned_by 'root' }
+# el6 and older
+if %w(redhat).include?(os[:family]) && os[:release] < '7.0'
+  describe file('/etc/sysconfig/network') do
+    it 'is a file' do
+      expect(subject).to be_file
+    end
+    it { should contain 'HOSTNAME=test.kitchen' }
+  end
 end
