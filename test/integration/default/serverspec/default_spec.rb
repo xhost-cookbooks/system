585c2f0e8f7eec860ee800723ed0df5ad4d958b9
Chris Fordham <chris.fordham@industrieit.com>
2015-05-27 21:02:08 +1000

    Group ServerSpec tests; add tests for /etc/hosts and the cron service.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,9 +5,6 @@ require_relative 'spec_helper'
 # /etc/hostname
 describe file('/etc/hostname') do
   it { should be_file }
-end
-
-describe file('/etc/hostname') do
   it { should be_owned_by 'root' }
 end
 
@@ -15,9 +12,6 @@ end
 if (os[:family] == 'debian') || (os[:family] == 'ubuntu')
   describe file('/etc/timezone') do
     it { should be_file }
-  end
-
-  describe file('/etc/timezone') do
     it { should be_owned_by 'root' }
   end
 
@@ -33,20 +27,41 @@ end
 
 describe host('localhost') do
   it { should be_resolvable.by('localhost') }
+  it { should be_reachable }
 end
 
 describe host('test.kitchen') do
   it { should be_resolvable.by('hosts') }
-end
-
-describe host('localhost') do
-  # ping
   it { should be_reachable }
 end
 
-describe host('test.kitchen') do
-  # ping
-  it { should be_reachable }
+# it's a little early to be able to expect most
+# host/guest environments to be able to do these
+# ipv6_hosts = %w(localhost6.localdomain6
+#                localhost6
+#                ip6-localhost
+#                ip6-loopback
+#                ip6-localnet
+#                ip6-mcastprefix
+#                ip6-allnodes
+#                ip6-allrouters)
+# ipv6_hosts.each do |host|
+#   describe host(host) do
+#     # ping
+#     it { should be_reachable }
+#     # resolution
+#     it { should be_resolvable.by('hosts') }
+#   end
+# end
+
+# /etc/hosts
+describe file('/etc/hosts') do
+  it { should contain('localhost').after('::1') }
+  it { should contain('localhost').after('127.0.0.1') }
+  it { should contain('localhost.localdomain').after('127.0.0.1') }
+  it { should contain('localdomain').after('127.0.0.1') }
+  it { should contain('chef.io').after('184.106.28.82') }
+  it { should contain('supermarket.io').after('95.211.29.66') }
 end
 
 # el6 and older
@@ -57,4 +72,20 @@ if %w(redhat).include?(os[:family]) && os[:release] < '7.0'
     end
     it { should contain 'HOSTNAME=test.kitchen' }
   end
+end
+
+# the cron service should be running
+case os[:family]
+when 'redhat', 'fedora'
+  cron_service_name = 'crond'
+when 'gentoo'
+  cron_service_name = 'vixie-cron'
+when 'arch'
+  cron_service_name = 'cronie'
+else
+  cron_service_name = 'cron'
+end
+
+describe service(cron_service_name) do
+  it { should be_running }
 end
