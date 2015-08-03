73b64dd33928fb0438d9ee473fb4b55106959391
Chris Fordham <chris.fordham@iag.com.au>
2015-08-03 15:08:15 +1000

    Initial support for system_environment.

---
+++ providers/environment.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+#
+# Cookbook Name:: system
+# Provider:: environment
+#
+# Copyright 2012-2015, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+action :configure do
+  template new_resource.filename do
+    variables(
+      extra: new_resource.extra
+    )
+  end
+
+  new_resource.updated_by_last_action(true)
+end
