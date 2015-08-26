5c859921b663a23e36a8cb3d521959ddd2785238
Chris Fordham <chris@fordham-nagy.id.au>
2015-08-26 19:01:11 +1000

    Mock ohai attribute node['network']['default_interface'] in rspec testing.

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -3,7 +3,11 @@
 require_relative 'spec_helper'
 
 describe 'system::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  let(:chef_run) do
+     ChefSpec::SoloRunner.new do |node|
+       node.set['network']['default_interface'] = 'eth0'
+     end.converge(described_recipe)
+  end
 
   it 'includes the `update_package_list` recipe' do
     expect(chef_run).to include_recipe('system::update_package_list')
