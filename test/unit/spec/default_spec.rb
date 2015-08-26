cae9c39a22de6c472813da8d81394e0218a78ea9
Chris Fordham <chris@fordham-nagy.id.au>
2015-08-26 19:03:09 +1000

    C: Use 2 (not 3) spaces for indentation.

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -4,9 +4,9 @@ require_relative 'spec_helper'
 
 describe 'system::default' do
   let(:chef_run) do
-     ChefSpec::SoloRunner.new do |node|
-       node.set['network']['default_interface'] = 'eth0'
-     end.converge(described_recipe)
+    ChefSpec::SoloRunner.new do |node|
+      node.set['network']['default_interface'] = 'eth0'
+    end.converge(described_recipe)
   end
 
   it 'includes the `update_package_list` recipe' do
