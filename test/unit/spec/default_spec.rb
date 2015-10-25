6f4792805530be303b46341745e29daee936394f
Chris Fordham <chris.fordham@rightscale.com>
2015-10-25 22:50:22 +1100

    Mock virtualzation/system attribute, stub ls command.

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -6,6 +6,8 @@ describe 'system::default' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new do |node|
       node.set['network']['default_interface'] = 'eth0'
+      node.set['virtualization']['system'] = 'kvm'
+      stub_command('ls /.dockerinit').and_return(false)
     end.converge(described_recipe)
   end
 
